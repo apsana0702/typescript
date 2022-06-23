6d39c6dd7bb30ddb419013f295c2210c029a7e47
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-23 15:14:54 +0530

    operator

---
+++ arithmatic operator.ts
@@ -0,0 +1,19 @@
+const message:string = "hello world!";
+console.log(message);
+var a:number=14;
+var b:number=6;
+var c:number=0;
+c=a+b;
+console.log("sum:"+c);
+c=a-b;
+console.log("diff:"+c);
+c=a*b;
+console.log("mul:"+c);
+c=a/b;
+console.log("div:"+c);
+c=a%b;
+console.log("remainder:"+c);
+c++;
+console.log("incr:"+c);
+c--
+console.log("decr:"+c);
