f291d3c9d5c76624aeb26cebea4462eb241d6dab
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-23 15:54:57 +0530

    for,while,do while

--- loop.ts
+++ loop.ts
@@ -8,3 +8,18 @@ for(let i=1;i<=4;i++){
         break;
     }
 }
+
+var a:number=3;
+ while(a<7)
+ {
+     console.log(a);
+     a++
+ }
+ var c:number=1;
+ do{
+     console.log(c);
+     c++;
+     if(c == 5){
+         break;
+     }
+ }while(c<9);
