bc757e9327e455b5121e40d5330df13570c1d03b
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-23 15:35:07 +0530

    operators

---
+++ bitwise assign.ts
@@ -0,0 +1,26 @@
+
+console.log(res);
+res+=k;var a:number=33;
+var b:number=46;
+ 
+var ans:boolean=((a>10)&&(b<50));
+console.log(ans);
+var ans:boolean=((a>70)||(b<10));
+console.log(ans);
+ans=a>b;
+console.log(ans);
+var k:number=2;
+var k2:number=3;
+var res;
+res=(k&k2);
+console.log(res);
+res=(k|k2);
+console.log(res);
+res=(k<<k2);
+console.log(res);
+res=(k>>k2);
+console.log(res);
+res+=k;
+console.log(res);
+res*=k2;
+console.log(res);
