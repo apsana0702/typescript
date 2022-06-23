5392fd2fe7deaab8937748c4e8591431ad10f72b
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-23 16:49:11 +0530

    Create functions.ts

---
+++ functions.ts
@@ -0,0 +1,16 @@
+function abc(){
+     console.log("this is function");
+ }
+ abc();
+ let anony=function abc(){
+     console.log("this is anonymous function");
+ }
+ anony();
+ let s=(a:number,b:number):number => {
+   console.log("arrow function");
+     console.log(a*b);
+ }
+  s(4,10);
+   
+  
+ 
