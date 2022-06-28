2f05161af16040da696ac5cd60d162678f13aac8
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-28 07:39:29 +0530

    Classes

---
+++ Classes.ts
@@ -0,0 +1,16 @@
+class flower{
+    name:string;
+    years:number;
+}
+const obj = new flower();
+obj.name='jasmine';
+obj.years=3;
+console.log (obj);
+class vehicle{
+    weight:number;
+    color:string;
+}
+const car= new vehicle();
+car.weight=3000;
+car.color="black";
+console.log (car)
