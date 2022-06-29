f7a45eb5afd21624fec4a1acae09657b92dcf9b2
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-29 08:05:51 +0530

    Class

--- Classes.ts
+++ Classes.ts
@@ -14,3 +14,77 @@ const car= new vehicle();
 car.weight=3000;
 car.color="black";
 console.log (car)
+
+
+
+
+// classes example
+
+class Invoice {
+ client: string;
+details: string;
+amount:number;
+
+constructor(c: string, d: string, a: number){
+
+this.client = c;
+
+this.details = d;
+
+this.amount = a;
+
+}
+
+format() {
+
+return `${this.client} owes £${this.amount} for ${this.details}`;
+
+}
+
+}
+
+const invOne = new Invoice('srija', 'buisness', 20);
+
+const invTwo = new Invoice('sravani', ' web developer', 30);
+
+let invoices: Invoice[] = [];
+
+invoices.push(invOne);
+
+invoices.push(invTwo);
+
+invOne.client = 'sanvi';
+
+invTwo.amount = 40;
+
+console.log(invOne, invTwo);
+
+console.log(invoices);
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+// inputs
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+
+const details = document.querySelector('#details') as HTMLInputElement;
+
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => { e.preventDefault();
+
+console.log(
+
+type.value,
+
+tofrom.value,
+
+details.value,
+
+amount.valueAsNumber
+
+);
+
+});
