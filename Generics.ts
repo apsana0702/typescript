486a1fd7d3fcb3eae9c466700197e875bce12c47
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-30 17:58:01 +0530

    Generics and adding constraints

--- Generics.ts
+++ Generics.ts
@@ -16,3 +16,9 @@ console.log (demo<number,number>(4,16));
 
 console.log (demo<string, number>("sri",90));
 
+
+function creatr<S extends string | number, T extends string | number>(v1: S, v2: T): [S, T] {
+  console.log(`creating pair: v1='${v1}', v2='${v2}'`);
+  return [v1, v2];
+}
+console.log (creatr("Hello",6));
